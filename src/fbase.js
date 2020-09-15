976d91faa5f8ae1e3aea579e81a1b2b1b1cc5031
sshim from desktop <mechurak@gmail.com>
2020-09-16 02:17:33 +0900

    initial commit

---
+++ src/fbase.js
@@ -0,0 +1,22 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+import "firebase/analytics"
+import "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+// export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const authService = firebase.auth();
