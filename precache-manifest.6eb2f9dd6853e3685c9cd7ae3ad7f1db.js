7a98580a2be949c51c5d1b9d4f8323ea8b96124a
sshim <mechurak@gmail.com>
2020-09-26 16:17:53 +0900

    Updates

---
+++ precache-manifest.6eb2f9dd6853e3685c9cd7ae3ad7f1db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb2b29a6ee2e29fa61ad05dc4206367",
+    "url": "/typing-practice-web/index.html"
+  },
+  {
+    "revision": "d894dc2e33168f73c028",
+    "url": "/typing-practice-web/static/css/main.9333dc4c.chunk.css"
+  },
+  {
+    "revision": "aeb99e884ff3d4ad33ac",
+    "url": "/typing-practice-web/static/js/2.7ce61644.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/typing-practice-web/static/js/2.7ce61644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d894dc2e33168f73c028",
+    "url": "/typing-practice-web/static/js/main.6083f392.chunk.js"
+  },
+  {
+    "revision": "d7e5ea17ea64df15cb9a",
+    "url": "/typing-practice-web/static/js/runtime-main.d603fe44.js"
+  }
+]);
