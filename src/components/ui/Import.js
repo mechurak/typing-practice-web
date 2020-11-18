848d88433d56285687134ddfa3c1a324f261db4b
sshim from series9 <mechurak@gmail.com>
2020-11-19 01:00:30 +0900

    Load sheet contents with url

--- src/components/ui/Import.js
+++ src/components/ui/Import.js
@@ -1,28 +1,46 @@
-import { useEffect, useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+import { useState } from "react";
 import Tabletop from "tabletop";
 
 export default function Practice() {
-  const [data, setData] = useState([]);
+  const [sheetUrl, setSheetUrl] = useState(
+    "https://docs.google.com/spreadsheets/d/1q9dzowKY0p5_Ee-OiCH2bbtTkycOmru-evlj9MsHr1E/edit#gid=284135557"
+  );
 
-  useEffect(() => {
+  const handleUrlChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setSheetUrl(value);
+  };
+
+  const loadSheet = (event) => {
+    console.log("loadSheet()", sheetUrl);
     Tabletop.init({
-      key: "1q9dzowKY0p5_Ee-OiCH2bbtTkycOmru-evlj9MsHr1E",
-      callback: (googleData) => {
-        setData(googleData);
+      key: sheetUrl,
+      callback: (data, tabletop) => {
+        const questions = tabletop.sheets("question").all();
+        const answers = tabletop.sheets("answer").all();
+        // TODO: Save it to localStorage
+        console.log(questions);
+        console.log(answers);
       },
       simpleSheet: true,
     });
-  }, []);
+  };
 
-  console.log("updated state --->", data);
   return (
     <div>
       <h3>Import!!!!</h3>
-      {data.map((element) => (
-        <div key={element.id}>
-          <p>{element.answer}</p>
-        </div>
-      ))}
+      <TextField
+        label="google sheet url"
+        defaultValue={sheetUrl}
+        fullWidth
+        onChange={handleUrlChange}
+      />
+      <Button variant="contained" color="primary" onClick={loadSheet}>
+        Import
+      </Button>
     </div>
   );
 }
